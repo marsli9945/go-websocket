6ef727246e08bcf07c739e0a55864661c2dca933
lilei <li8369945>
2020-10-21 20:24:06 +0800

    打印request_id

--- logger/action.go
+++ logger/action.go
@@ -1,14 +1,10 @@
 package logger
 
 import (
-	"bytes"
 	"encoding/json"
 	"github.com/marsli9945/go-websocket/form"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
-	"time"
 )
 
 var gapi_host = os.Getenv("GAPI_HOST") + "/api/ga/v1/grow-analytics-log-server/log/send"
@@ -24,15 +20,15 @@ func Push(event string, param form.SendForm) {
 	}
 
 	// 超时时间：5秒
-	client := &http.Client{Timeout: 5 * time.Second}
+	//client := &http.Client{Timeout: 5 * time.Second}
 	log.Println("gapi_host:{}", gapi_host)
 	log.Println("jsonStr:{}", string(jsonStr))
-	resp, err := client.Post(gapi_host, "application/json;charset=utf-8;", bytes.NewBuffer(jsonStr))
-	defer resp.Body.Close()
-	if err != nil {
-		log.Println(err)
-		resp.Body.Close()
-	}
-	body, _ := ioutil.ReadAll(resp.Body)
-	log.Println("body:{}", string(body))
+	//resp, err := client.Post(gapi_host, "application/json;charset=utf-8;", bytes.NewBuffer(jsonStr))
+	//defer resp.Body.Close()
+	//if err != nil {
+	//	log.Println(err)
+	//	resp.Body.Close()
+	//}
+	//body, _ := ioutil.ReadAll(resp.Body)
+	//log.Println("body:{}", string(body))
 }
