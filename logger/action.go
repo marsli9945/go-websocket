a1472782e33c41883aecb0eb1bc9d8b82217befe
lilei <li8369945>
2020-11-05 10:17:13 +0800

    上报日志换自己的接口 不信那小子了

--- logger/action.go
+++ logger/action.go
@@ -25,7 +25,7 @@ func Push(event string, param form.SendForm) {
 	properties := NewProperties(param.Project_id, param.Model_name, param.Request_id)
 	logContent := NewLogContent(event, param.Device_id, param.User_id, *properties, *lib)
 	logList = append(logList, logContent)
-	if len(logList) < 2 {
+	if len(logList) < 20 {
 		return
 	}
 
